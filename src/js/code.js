ba114ec7133b7f683024babe77a9626876c2e0c0
Nikolay Samchuk <niksam9419rus@gmail.com>
2023-04-18 20:04:35 +0700

    восстановление после неудачного мерджа

--- src/js/code.js
+++ src/js/code.js
@@ -16,43 +16,45 @@ $('[data-fancybox="gallery"]').fancybox({
     'width': '600px',
 });
 
+
 /************табы в секции "Услуги и цены"************/
-const tabPriceArray = Array.from(document.querySelectorAll('a.price')); //массив ссылок на услуги
-const priceArray = Array.from(document.querySelectorAll('div.price')); //массив услуг
+const list = document.querySelectorAll('a.price');
+const box = Array.from(list);
 
-tabPriceArray[0].style = 'text-decoration: underline; color: #00BF78'; //отображение на старте
-priceArray[0].style = 'display: flex';
+const list1 = document.querySelectorAll('div.price');
+const box1 = Array.from(list1);
 
-tabPriceArray.forEach(function (num, i) { //пробегаем по массиву ссылок
-    num.addEventListener("click", function () { //если click
-        for (let j = 0; j < tabPriceArray.length; j++) {
+box.forEach(function (num, i) {
+    num.addEventListener("click", function () {
+        for (let j = 0; j < 6; j++) {
             if (i === j) {
-                tabPriceArray[j].style = 'text-decoration: underline; color: #00BF78';
-                priceArray[j].style = 'display: flex'; //отображение услуг
+                box1[j].style = 'display: flex';
+                box[j].style = 'text-decoration: underline; color: #00BF78';
             }
-            else { 
-                tabPriceArray[j].style = 'text-decoration: none; color: #333333';
-                priceArray[j].style = 'display: none'; //скрытие услуг
+            else {
+                box1[j].style = 'display: none';
+                box[j].style = 'text-decoration: none; color: #333333';
             }
         }
     });
 });
 
+
 /************плавный скролл"************/
 document.querySelectorAll('a.scroll').forEach(link => {
-    link.addEventListener('click', function (smoothScroll) {
-        smoothScroll.preventDefault(); 
-        const getHref = this.getAttribute('href').substring(1); //получаем ссылку без #
-        const scrollTarget = document.getElementById(getHref); //элемент по ссылке
-        const topOffset = 100; //задаем смещение
-        const elementTop = scrollTarget.getBoundingClientRect().top; //верхняя граница выбранного элемента
-        const offsetPosition = elementTop - topOffset; //смещение
-        window.scrollBy({ //делаем скролл
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const getHref = this.getAttribute('href').substring(1)
+        const scrollTarget = document.getElementById(getHref)
+        const topOffset = 100
+        const elementTop = scrollTarget.getBoundingClientRect().top
+        const offsetPosition = elementTop - topOffset
+        window.scrollBy({
             top: offsetPosition,
             behavior: 'smooth',
-        });
-    });
-});
+        })
+    })
+})
 
 /************данные с формы"************/
 const contactsForm = document.getElementsByClassName('form');
